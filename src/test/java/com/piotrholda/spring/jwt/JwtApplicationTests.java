9db8742a0b4d286b814b5e309746d1630654f563
phold <pholda@gmail.com>
2023-11-08 19:13:45 +0100

    DB Connection

--- src/test/java/com/piotrholda/spring/jwt/JwtApplicationTests.java
+++ src/test/java/com/piotrholda/spring/jwt/JwtApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-class ApplicationTests {
+class JwtApplicationTests {
 
 	@Test
 	void contextLoads() {
